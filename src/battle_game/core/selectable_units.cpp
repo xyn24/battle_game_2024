50bdaf86829a0946c624199267b11eced2707ac2
Xu YiNuo <xyn24@mails.tsinghua.edu.cn>
2025-01-09 19:53:11 +0800

    add_elec_tank

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -21,6 +21,7 @@ void GameCore::GeneratePrimaryUnitList() {
   /*
    * TODO: Add Your Unit Here!
    * */
+  ADD_SELECTABLE_UNIT(unit::ElecTank_ng57683);
   ADD_SELECTABLE_UNIT(unit::Tank);
 
   unit.reset();
